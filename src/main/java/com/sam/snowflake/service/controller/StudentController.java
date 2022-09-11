5e82231bee1dbf4463c8a9f550d9a5230860fdbe
samckp <sam.mca.liet@gmail.com>
2022-09-11 11:38:38 +0530

    updated

--- src/main/java/com/sam/snowflake/service/controller/StudentController.java
+++ src/main/java/com/sam/snowflake/service/controller/StudentController.java
@@ -1,2 +1,25 @@
-package com.sam.snowflake.service.controller;public class StudentController {
+package com.sam.snowflake.service.controller;
+
+import com.sam.snowflake.service.entity.Student;
+import com.sam.snowflake.service.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/")
+public class StudentController {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @GetMapping("students")
+    public ResponseEntity<List<Student>> getStudents(){
+
+        return new ResponseEntity<>((List<Student>) studentRepository.findAll(), HttpStatus.OK);
+    }
+
 }
